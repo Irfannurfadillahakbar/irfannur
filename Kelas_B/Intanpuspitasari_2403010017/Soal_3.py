c695df0a9ed9a6cad2dfb930d38a10fee6157ee2
unknown <intaanpuspita-sari12>
2024-11-11 09:53:44 +0700

    menambahkan jawaban uts praktikum

---
+++ Kelas_B/Intanpuspitasari_2403010017/Soal_3.py
@@ -0,0 +1,6 @@
+#menggabungkan 2 kalimat tnpa spasi --> intanps-2403010017
+kata1=str(input("masukan kata pertama:"))
+kata2=str(input("masukan kata kedua:"))
+gabungan= kata1+kata2
+spasi= gabungan.replace(" ", "")
+print("kata yang anda masukan adalah:" ,spasi)
---
+++ Kelas_B/Intanpuspitasari_2403010017/Soal_5.py
@@ -0,0 +1,6 @@
+#memotong substring dari kata --> intanps-2403010017
+kata=str(input("masukan kalimat anda:"))
+indeksawal=int (input("masukan indeks awal:"))
+indeksakhir=int (input("masukan indeks akhir:"))
+substring=kata[indeksawal:indeksakhir]
+print(substring)
