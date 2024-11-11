40db6378680a47fd0c90dd9daf33532d8dcbbb3c
Nab1ll <2403010046@unper.ac.id>
2024-11-11 15:25:54 +0700

    sudah selesai

--- Kelas_B/tugas_3.py
+++
@@ -1,5 +0,0 @@
-kata_pertama = str(input("masukan kata pertama"))
-kata_kadua = str(input("masukan kata kedua"))
-gabungkan_kata = kata_pertama + kata_kadua
-spasi = gabungkan_kata.replace(" ","")
-print("kata yang anda masukan",spasi)
