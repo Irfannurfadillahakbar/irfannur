8ab8cde6410b00ce16c9157abfdb78af7ca4f32b
sakibfaturrahman303 <2403010015@unper.ac.id>
2024-11-11 13:57:43 +0700

    Menambahkan jawaban UTS Praktikum Alpro

---
+++ Kelas_A/Sakib_Faturrahman_2403010015/Soal_1.py
@@ -0,0 +1,10 @@
+#Sakib Faturrahman
+#Nim : 2403010015
+#disini saya menghitung luas dan keliling persegi panjang dengan menggunakan bahasa pemrograman python dengan inputan panjang dan lebar dari user
+#dengan tipe data float untuk menghitung luas dan keliling
+panjang = input("Masukkan panjang: ")
+lebar = input("Masukkan lebar: ")
+luas = float(panjang) * float(lebar)
+kelilingan = 2 * (float(panjang) + float(lebar))
+print("Luas persegi panjang: ", luas)
+print("Keliling persegi panjang: ", kelilingan)
---
+++ Kelas_A/Sakib_Faturrahman_2403010015/Soal_2.py
@@ -0,0 +1,10 @@
+#Sakib Faturrahman
+#Nim : 2403010015
+#disini saya menghitung konversi celcius ke fahrenheit dengan menggunakan bahasa pemrograman python 
+# dengan inputan suhu dalam celcius dari user menggunakan tipe data float
+
+suhu_celcius = input("Masukkan suhu dalam suhu_celcius: ")
+
+suhu_fahrenheit = float(suhu_celcius) * 1.8 + 32
+
+print("Suhu dalam fahrenheit: ", suhu_fahrenheit)
