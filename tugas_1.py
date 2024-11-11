7f48b6787d4d81be4382eca0ae431ce39710c972
Nab1ll <2403010046@unper.ac.id>
2024-11-11 15:30:42 +0700

    hapus

--- tugas_1.py
+++
@@ -1,6 +0,0 @@
-panjang=float(input("masukan panjang"))
-lebar=float(input("masukan lebar"))
-luas=panjang*lebar
-keliling=2*(panjang + lebar)
-print("ukuran luas,luas")
-print("ukuran keliling",keliling)
--- tugas_2.py
+++
@@ -1,5 +0,0 @@
-celcius = float(input("input celcius: "))
-
-#konversi celcius ke farenheat
-farenheat = ((9/5) * celcius ) + 32
-print("suhu farenheat adalah", farenheat,"farenheat")
