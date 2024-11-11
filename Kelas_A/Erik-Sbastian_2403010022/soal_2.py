abbe257a4735931f8f7992d36488f3df961c44b3
Sbastiann <2403010022@unper.ac.id>
2024-11-11 14:45:32 +0700

    pertama

---
+++ Kelas_A/Erik-Sbastian_2403010022/soal_2.py
@@ -0,0 +1,11 @@
+# Erik Sbastian TI 24 A
+# Mengkonversi Celcius ke farhenheit
+
+# Meminta pengguna memasukkan suhu dalam celcius
+celcius = float(input("Masukkan Suhu Dalam Celcius: "))
+
+# Konversi celcius ke farhenheit
+f = (celcius * 9/5) + 32
+
+# Menampilkan hasil
+print(f"Suhu dari {celcius} Celcius, adalah: ", f)
---
+++ Kelas_A/Erik-Sbastian_2403010022/tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+# Erik Sbastian TI 24 A
+# Menghitung luas dan keliling persegi panjang
+
+# Input Panjang dan lebar
+panjang = float(input("Masukkan Panjang: "))
+lebar = float(input("Masukkan Lebar: "))
+
+# Menghitung Luas
+luas = panjang * lebar
+
+# Menghitung keliling
+keliling = 2 * (panjang + lebar)
+
+# Menampilkan Hasil
+print("Luas persegi panjang tersebut adalah: ", luas)
+print("Keliling persegi panjang tersebut adalah: ", keliling)
