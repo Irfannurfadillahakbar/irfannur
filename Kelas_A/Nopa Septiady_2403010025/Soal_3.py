4a236505f474ec11734c7918604e7a35170c64b7
pekyu02 <2403010025@unper.ac.id>
2024-11-11 14:51:49 +0700

    Menambahkan jawaban UTS Praktikum

---
+++ Kelas_A/Nopa Septiady_2403010025/Soal_3.py
@@ -0,0 +1,7 @@
+#Nopa Septiady
+#2403010025
+#Menggabungkan dua kata, menjadikan satu kalimat
+
+kalimat_1 = input("masukan string kesatu: ")
+kalimat_2 = input("masukan string kedua: ")
+print(kalimat_1 + "" + kalimat_2)
---
+++ Kelas_A/Nopa Septiady_2403010025/Soal_5.py
@@ -0,0 +1,11 @@
+#Nopa septiady
+#2403010025
+#Membuat substring dari sebuah kata dengan bahasa pemograman
+
+kalimat = input("Masukan Kalimat: ")
+
+index_awal = int(input("Masukan indeks index_awal: "))
+
+index_akhir = int(input("Masukan indeks index_akhir: "))
+
+print(kalimat[index_awal:index_akhir])
