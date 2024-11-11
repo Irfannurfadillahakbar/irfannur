f67c2199be52f927fc3818de4844a295bd483ad6
Rafliansyah-lightborn <2403010002@unper.ac.id>
2024-11-11 16:28:50 +0700

    PR Rafliansyah Aushaf - 2403010002

---
+++ Kelas_A/Rafliansyah Aushaf - 2403010002/soal_1.py
@@ -0,0 +1,7 @@
+#Menghitung Luas dan Keliling Persegi Panjang
+panjang = float(input("Masukan Panjang: "))
+lebar = float(input("Masukan Lebar: "))
+luas = panjang * lebar
+keliling = 2 * (panjang + lebar)
+print("luas persegi panjang", luas)
+print("keliling persegi panjang", keliling)
