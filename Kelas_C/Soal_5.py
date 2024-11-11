5fae12c724e8e5ad2bc2a0eb97f04e1c5ccd4ed7
Kasyfirrabbani <2403010082@unper.ac.id>
2024-11-11 13:46:43 +0700

    Create Soal_5.py

---
+++ Kelas_C/Soal_5.py
@@ -0,0 +1,18 @@
+a = "Hello, World!"
+print(a[2])               
+print(len(a))             
+print(a.upper())           
+print(a.lower())          
+print(a.replace("H", "G")) 
+print(a[2:5])             
+print(a[7:10])            
+print(a.count("O"))       
+hasilnya:
+l
+13
+HELLO, WORLD!
+hello, world!
+Gello, World!
+llo
+Wor
+2
