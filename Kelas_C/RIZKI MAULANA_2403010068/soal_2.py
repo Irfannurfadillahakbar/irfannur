5d9b063a42d4ba4bca4d233d9ca1b72700423bb8
RizkiMaulanaUnper <2403010068@unper.ac.id>
2024-11-11 08:56:25 +0700

    Add files via upload

---
+++ Kelas_C/RIZKI MAULANA_2403010068/soal_2.py
@@ -0,0 +1,14 @@
+# Nama : Rizki Maulana
+# Nim : 2403010068
+# Program Konversi Suhu dari Celcius ke Fahrenheit
+
+# Meminta input dari pengguna untuk suhu dalam Celcius
+celcius = float(input("Masukkan suhu dalam Celcius: "))
+
+# Mengonversi suhu dari Celcius ke Fahrenheit menggunakan rumus
+fahrenheit = (celcius * 9 / 5) + 32
+
+# Menampilkan hasil konversi
+print("Suhu dalam Fahrenheit:", fahrenheit)
+
+#maaf pak untuk soal no 2 saya googling 
