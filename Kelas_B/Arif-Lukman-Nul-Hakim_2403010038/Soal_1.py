4bddb621f142272d4db0c3c4c337b178fc926052
Ariflukmnl <2403010038@unper.ac.id>
2024-11-11 18:56:08 +0700

    Delete Kelas_B/Arif-Lukman-Nul-Hakim_2403010038/Soal_1.py

--- Kelas_B/Arif-Lukman-Nul-Hakim_2403010038/Soal_1.py
+++
@@ -1,6 +0,0 @@
-panjang=float(input("masukkan panjang persegi panjang:"))
-lebar=float(input("masukkan lebar persegi panjang:"))
-luas=panjang*lebar
-keliling=2*(panjang+lebar)
-print("luas persegi panjang:",luas)
-print("keliling persegi panjang:",keliling)
