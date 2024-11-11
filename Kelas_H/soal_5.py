9b5a3bb9a8a21c87e7992d371496544e0828b2ea
nada928 <2403010105@unper.ac.id>
2024-11-11 21:06:44 +0700

    Create soal_5.py
    Masukkan kata: Algoritma
    Masukkan indeks awal: 2
    Masukkan indeks akhir: 6
    Substring: gori

---
+++ Kelas_H/soal_5.py
@@ -0,0 +1,11 @@
+# Program untuk memotong substring dari sebuah kata
+# Meminta input kata, indeks awal dan indeks akhir
+kata = input("Masukkan kata: ")
+indeks_awal = int(input("Masukkan indeks awal: "))
+indeks_akhir = int(input("Masukkan indeks akhir: "))
+
+# Mengambil substring dari kata berdasarkan indeks
+substring = kata[indeks_awal:indeks_akhir]
+
+# Menampilkan hasil substring
+print("Substring:", substring)
