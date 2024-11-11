b5ed3ce38381d31b8f3df9081af8cc4b83b0dc0e
NadiaSahabillaAzZahra <2403010011@unper.ac.id>
2024-11-11 15:54:44 +0700

    Hapus mahasiswa lain

--- Kelas_B/2403010053 M RABBANI M/soal_3.py
+++
@@ -1,5 +0,0 @@
-kata_pertama = str(input("masukan kata pertama"))
-kata_kedua = str(input("masukan kata kedua"))
-gabungkan_kata = kata_pertama + kata_kedua
-spasi = gabungkan_kata.replace(" ","")
-print("kata yang anda masukan",spasi)
