ea39264ee7db97b05a0dd767551e5311e5d09351
FeriRifandi <2403010221@unper.ac.id>
2024-11-11 18:16:12 +0700

    Delete Kelas_H/JAWABAN UTS FERI RIFANDI NO 2.py

--- Kelas_H/JAWABAN UTS FERI RIFANDI NO 2.py
+++
@@ -1,8 +0,0 @@
-# Meminta input suhu dalam Celcius dari pengguna
-celcius = float(input("Masukkan suhu dalam Celcius: "))
-
-# Mengonversi Celcius ke Fahrenheit
-fahrenheit = (celcius * 9/5) + 32
-
-# Menampilkan hasil
-print(f"{celcius}°C sama dengan {fahrenheit}°F")
