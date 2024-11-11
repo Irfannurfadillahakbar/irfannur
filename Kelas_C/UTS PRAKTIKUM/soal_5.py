5ea9febe9f32b77953fc4500599e2e8481b57ec2
RizkiMaulanaUnper <2403010068@unper.ac.id>
2024-11-11 08:34:28 +0700

    Add files via upload

---
+++ Kelas_C/UTS PRAKTIKUM/soal_5.py
@@ -0,0 +1,16 @@
+# Nama : Rizki Maulana
+# Nim : 2403010068
+#  Menmotong kta
+
+depan = input("Masukkan Nama Depan Nada : ")
+belakang = input("Masukan Nama Belakang Anda :")
+
+
+
+
+print("Nama Lengkap Anda ::"+" "+depan,belakang)
+
+lengkap = depan + belakang
+
+substring = lengkap[2:6]  
+print(substring)
