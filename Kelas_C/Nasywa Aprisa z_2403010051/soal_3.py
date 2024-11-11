233d24a643b6716cc5e858e36faf36f3998bee8e
Awaanaz <2403010051@unper.ac.id>
2024-11-11 21:27:24 +0700

    Add files via upload

---
+++ Kelas_C/Nasywa Aprisa z_2403010051/soal_3.py
@@ -0,0 +1,9 @@
+#promt the user for the first and second words
+kata1 = input("Nama Depan: ")
+kata2 = input("Nama Belakang: ")
+
+#combine the word without space
+result = kata1 + kata2
+
+#output the result
+print("Nama Lengkap:", result)
