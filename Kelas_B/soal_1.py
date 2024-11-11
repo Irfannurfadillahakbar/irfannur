6eeb2e1b8c8f264cc36d8a96729c45e06020d021
Rakk4 <2403010056@unper.ac.id>
2024-11-11 15:13:54 +0700

    done

--- Kelas_B/soal_1.py
+++
@@ -1,8 +0,0 @@
-
-
-panjang=float(input("masukan panjang"))
-Lebar=float(input("masukan lebar"))
-Luas= panjang*Lebar
-Keliling= 2*(panjang+Lebar)
-print(f'luas dari{panjang}*{Lebar}={Luas}')
-print(f'Keiling dari{panjang}dan{Luas}adalah{Keliling}')
--- Kelas_B/soal_3.py
+++
@@ -1,5 +0,0 @@
-kata_pertama = str(input("Masukan kata pertama"))
-kata_kedua = str(input("Masukan kata kedua"))
-gabungkan_kata = kata_pertama + kata_kedua
-spasi = gabungkan_kata.replace(" ", "")
-print("kata yang anda masukan",spasi)
