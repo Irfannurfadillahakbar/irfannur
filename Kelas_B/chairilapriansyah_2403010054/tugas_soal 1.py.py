ed985a370ea0f5746ff5c97fd4e50ccf0b88f644
chairilapriansyah <2403010054@unper.ac.id>
2024-11-11 18:42:51 +0700

    chairilapriansyah_2403010054

---
+++ Kelas_B/chairilapriansyah_2403010054/tugas_soal 1.py.py
@@ -0,0 +1,6 @@
+panjang=float("7")
+lebar=float("4")
+luas= panjang*lebar
+keliling= 2*(panjang+lebar)
+print(f'luas dari{panjang}={lebar}={luas}')
+print(f'keliling dari{panjang}dan{luas}adalah{keliling}')
---
+++ Kelas_B/chairilapriansyah_2403010054/tugas_soal 3.py.py
@@ -0,0 +1,5 @@
+a=str("chairil")
+b=str("apriansyah")
+gabungkan_kata = a + b
+spsi = gabungkan_kata.replace(" "," ")
+print("kata yang anda masukan",spsi)
