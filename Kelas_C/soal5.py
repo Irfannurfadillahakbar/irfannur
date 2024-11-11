8a688bc01dd86b2b10e103ce289b779b7ff61454
rahminurfadilah24 <2403010043@unper.ac.id>
2024-11-11 20:57:43 +0700

    ulangan alpro

---
+++ Kelas_C/soal5.py
@@ -0,0 +1,11 @@
+# Program to extract a substring based on given start and end indices
+
+# Get inputs from the user
+word = input("Masukkan kata: ")
+start_index = int("input(masukan indeks awal: ")
+end_index = int(input("masukkan indexs akhir: "))
+# Get the substring
+substring = word[start_index:end_index]
+
+# Display the result
+print("substring:, substring")
