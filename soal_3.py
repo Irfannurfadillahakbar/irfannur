54eb2b80712a85cc18ec3f0d62cf657c24ba7db2
DreamBlack-ff <2403010005@unper.ac.id>
2024-11-11 16:51:16 +0700

    DIDA_FERDIANA_2403010005

---
+++ soal_3.py
@@ -0,0 +1,7 @@
+# Dida Ferdiana 2403010005
+# input string 1 dan 2
+string1 = input ("masukkan string 1: ")
+string2 = input ("masukkan string 2: ")
+
+# output
+print(string1 + string2)
---
+++ soal_5.py
@@ -0,0 +1,5 @@
+# Dida Ferdiana 2403010005
+kata = input ("Masukan kata: ")
+indeks_awal = int (input ("Masukan indeks awal: "))
+indeks_akhir = int (input ("Masukan indeks akhir: "))
+print (kata [indeks_awal:indeks_akhir])
