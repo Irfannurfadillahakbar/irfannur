3b389551617ba578ae39adce781dedecf02fb99b
yolanovitawati <2403010020@unper.ac.id>
2024-11-11 21:25:12 +0700

    yolanovitawati_2403010020

---
+++ Kelas_B/yolanovitawati_2403010020/Soal_3.py
@@ -0,0 +1,5 @@
+kata_pertama=str(input("Masukan kata pertama: "))
+kata_kedua=str(input("Masukan kata kedua: "))
+gabungan_kata=kata_pertama+kata_kedua
+spasi=gabungan_kata.replace(" "," ")
+print(f"Hasil penggabungan",spasi)
